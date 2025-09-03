30d6182ff79a539092a3614db06568134542744e
Mikiyas Woubshet <mikiyaswoubshet@gmail.com>
2025-09-02 23:42:20 -0700

    Testimonials

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Meals from './components/Meals'
 import ToggleBackgroundColor from './components/ToggleBackgroundColor'
 import HiddenSearchBar from './components/HiddenSearchBar'
+import Testimonials from './components/Testimonials'
 
 const App = () => {
   return (
     <div>
-    /* <Meals/>
-    <ToggleBackgroundColor/> */
+    <Meals/>
+    <ToggleBackgroundColor/> 
     <HiddenSearchBar/>
+    <Testimonials/>
     </div>
   )
 }
---
+++ src/components/Testimonials.jsx
@@ -0,0 +1,47 @@
+import React, { useState } from 'react'
+
+const Testimonials = () => {
+    const [currentIndex, setCurrentIndex]=useState(0);
+    const testimonials=[
+        {
+            quote:'This is the best product i have ever used',
+            auther:'jane doe'
+         },
+        {
+            quote:'I recommend this product to everyone',
+            auther:'Mike Woub'
+         },
+        {
+            quote:'wow what a product to have on your hands',
+            auther:'betty Fekadu'
+         },
+        
+                    ]
+    const handlePrev=()=>{
+        setCurrentIndex((currentIndex + testimonials.length-1) % testimonials.length);
+    }
+    const handleNext=()=>{
+        setCurrentIndex((currentIndex + testimonials.length+1) % testimonials.length);
+    }
+  return (
+    
+    <div className='testimonials-container'>
+    
+        <div className='testimonials'>
+            <div className='testimonials-quote'>
+                {testimonials[currentIndex].quote}
+            </div>
+            <div className='testimonials-auther'>
+                {testimonials[currentIndex].auther}
+            </div>
+            <div className='testimonials-nav'>
+                <button onClick={handlePrev}>prev</button>
+                <button onClick={handleNext}>next</button>
+        </div>
+    </div>
+
+    </div>
+  )
+}
+
+export default Testimonials
